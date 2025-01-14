dcce16179bbf72ce1e71ab82bdd0d11d62ef43a1
Starlee Jiles <starlee.jiles@icloud.com>
2025-01-14 22:49:43 +0000

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greeting from './Greeting';
+import SocialLinks from './SocialLinks';
+
+function Header() {
+  return <nav>
+      <Greeting />
+      <SocialLinks />
+  </nav>
+}
+
+export default Header;
