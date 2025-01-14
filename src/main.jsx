ab126f8002476c8aec6b16ffef185cdf5e63787f
Starlee Jiles <starlee.jiles@icloud.com>
2025-01-14 22:14:57 +0000

    render the app component to the root element

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
 import "./index.css";
-
+import ReactDOM from "react-dom/client";
+import App from "./App";
 // Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(<App />);
