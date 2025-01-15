25dceaa8d80fe00a1fda435eab0e774d0853b80f
Starlee Jiles <starlee.jiles@icloud.com>
2025-01-15 04:44:26 +0000

    add my headshot and tweak my bio

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,9 +10,9 @@ function AboutMe() {
         style={{width: '150px', borderRadius: '50%'}}
       />
       <p>
-      Hello! My name is Starlee Jiles, and I am studying software engineering at Atlas School with a specialization in full-stack web development. My favorite technologies include React, Python, and MongoDB, as I enjoy building dynamic web applications. 
+      Hello! My name is Starlee Jiles, and I am currently in my 5th trimester at Atlas School studying software engineering with a specialization in full-stack web development. My favorite technologies include React, Python, and MongoDB, as I enjoy building dynamic web applications. 
       After graduation, I aspire to work as either a full-stack web developer or potentially a project manager, building impactful applications that make a difference.
-      When I’m not coding, I lead a young adults group and plan events for us to foster community. I also enjoy spending time with family, friends, and going hiking with my dogs!
+      When I’m not coding, I love fostering community through a young adults group that I lead. I also enjoy spending time with family, friends, and going hiking with my dogs!
       I’m always excited to learn and grow, both professionally and personally!
       </p>
     </div>
