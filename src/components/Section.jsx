b8d6fd26da4478dbf8926c7c43e93e344e96ed76
Starlee Jiles <starlee.jiles@icloud.com>
2025-01-15 02:00:24 +0000

    create section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Section({ title, children}) {
+  return (
+    <div className="section">
+      <h1>{title}</h1>
+      {children}
+    </div>
+  );
+}
+
+export default Section;
