25c042fb8937ab6f59ca7da4f41d41911359433a
Starlee Jiles <starlee.jiles@icloud.com>
2025-01-15 02:04:58 +0000

    add section into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import Section from './components/Section';
 
 function App() {
   return (
     <div>
       <Header />
+      <Section title="What is React?">
+        <p>
+        React is a JavaScript library for building user interfaces. It allows developers to create reusable UI components, manage the state of their application effectively, and efficiently update the DOM when data changes. React is widely used for its speed, simplicity, and flexibility.
+        </p>
+      </Section>
     </div>
   );
 }
